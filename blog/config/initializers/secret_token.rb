710a6a7190c4e87a25d133edf18de50d21b0ff1c
Anny K. Ly <annykim.ly@gmail.com>
2014-02-18 00:41:07 -0500

    Basic Blog Website
    http://guides.rubyonrails.org/getting_started.html

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '2232d6c940649d2b0fbc0a194b20d6a06b16d03dc273280eb3a542b9f180470dcbc90c3beca4b65741d7bf66d01c8b0f9433515ed65f4c0d803fcd0b40732a60'
